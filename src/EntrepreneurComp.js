86163f4d3a07571705a46fdb92a2e36f350fd8a4
Elnar  Sharifli <esharifli@homit.ca>
2020-03-28 11:45:42 -0600

    Our-Problem component completed

--- src/EntrepreneurComp.js
+++ src/EntrepreneurComp.js
@@ -6,18 +6,18 @@ import KS from './images/pages/kevin_systrom.jpg'
 
 export default function EntrepreneurComp() {
     return (
-        <div className='entrepreneur-comp'>
+        <div className="entrepreneur-comp">
             <div className="comp-header">
                 <text>Entrepreneur Advice</text>
                 <div></div>
             </div>
             <div className="comp-body">
-                <img src={PG} className='entr-img' />
+                <img src={PG} className="entr-img" />
                 <p>"…It sounds obvious to say you should only work on problems that exist. And yet by far the most common mistake startups make is to solve problems no one has."</p>
             </div>
             <div className="comp-body">
                 <p>“…Hard part is actually finding a problem to solve. Solutions come pretty easily for the majority of problems”</p>
-                <img src={KS} className='entr-img' />
+                <img src={KS} className="entr-img" />
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './Header';
 import EntrepreneurComp from './EntrepreneurComp';
+import OurProblem from './OurProblem';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header/>
     <EntrepreneurComp/>
+    <OurProblem/>
   </React.StrictMode>,
   document.getElementById('root')
 );
