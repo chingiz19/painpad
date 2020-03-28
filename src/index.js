c86000e5411619750b3d1131f8d339efdf5b77c1
Chingiz <chingiz.bakhishov@gmail.com>
2020-03-28 17:47:06 -0600

    Add analytics; Fix signup part

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './Header';
+import Tracker from './MixPanelTracker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header/>
+    <Tracker />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
