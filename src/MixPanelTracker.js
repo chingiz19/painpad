051394df1c0414c3f9df82c6af52c6eab9591313
Chingiz <chingiz.bakhishov@gmail.com>
2020-03-28 17:49:25 -0600

    Trying to fix gitlab error issue

--- src/MixPanelTracker.js
+++ src/MixPanelTracker.js
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 const Tracker = (props) => {
     mixpanel.init("c4db1142f0a03880749300f3dff497dd");
 
+    //tracking for the only event
     mixpanel.track('Page visit', { identifier: uuidv4() });
 
     return (
