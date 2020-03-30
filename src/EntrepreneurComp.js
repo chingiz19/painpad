c653ca50c5b2b62f06a0c832eae7a1934e7d5933
Chingiz <chingiz.bakhishov@gmail.com>
2020-03-30 16:12:05 -0600

    Remove warnings

--- src/EntrepreneurComp.js
+++ src/EntrepreneurComp.js
@@ -12,12 +12,12 @@ export default function EntrepreneurComp() {
             <div className="entrepreneur-comp">
                 <ComponentHeader contentText="Entrepreneur Advice" />
                 <div className="comp-body PG-div">
-                    <img src={PG} className="entr-img" />
+                    <img src={PG} alt='something' className="entr-img" />
                     <p>"…It sounds obvious to say you should only work on problems that exist. And yet by far the most common mistake startups make is to solve problems no one has."</p>
                 </div>
                 <div className="comp-body KS-div">
                     <p>“…Hard part is actually finding a problem to solve. Solutions come pretty easily for the majority of problems”</p>
-                    <img src={KS} className="entr-img" />
+                    <img src={KS} alt='something' className="entr-img" />
                 </div>
             </div>
         </Fade>
