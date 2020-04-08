8468175d7c8f26199b859e7733902741f27cf7d4
Elnar  Sharifli <esharifli@homit.ca>
2020-04-08 00:31:14 -0600

    sign in functionality and data validation added

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import Validate from 'validate.js';
 import './SignIn.css';
+import TextField from '@material-ui/core/TextField';
 
 export default function SignIn() {
-    return (
-        <>
-            <h1>Sign In here</h1>
-        </>
-    );
+    const username = useRef(null);
+    const password = useRef(null);
+
+    const constraints = {
+        username: {email: {
+            message: "doesn't look like a valid email"
+          }
+        },
+        password: {
+            length: {
+                minimum: 6,
+                tooShort: "needs to have %{count} letters or more",
+            }
+        }
+};
+
+const [{ emailMessage, passMessage }, setMessage] = useState({ emailMessage: null, passMessage: null });
+
+const submitInput = e => {
+    let check = Validate({
+        username: username.current.value,
+        password: password.current.value
+    }, constraints);
+
+    setMessage({
+        emailMessage: check && check.username ? check.username[0] : null,
+        passMessage: check && check.password ? check.password[0] : null,
+    });
+}
+
+return (
+    <>
+        <div className="signin-main-div">
+            <TextField required
+                error={emailMessage != null}
+                id="signin-email"
+                label="Email"
+                name="email"
+                inputRef={username}
+                helperText={emailMessage}
+                variant="outlined"
+                size="small"
+                type="email" />
+
+            <TextField required
+                error={passMessage != null}
+                id="signin-password"
+                label="Password"
+                name="email"
+                inputRef={password}
+                helperText={passMessage}
+                variant="outlined"
+                size="small"
+                type="password" />
+            <button onClick={submitInput}>Submit</button>
+        </div>
+    </>
+);
 }
--- src/Modals/UserSignInUp.js
+++ src/Modals/UserSignInUp.js
@@ -4,8 +4,6 @@ import SignIn from '../Components/SignIn'
 import SignUp from '../Components/SignUp'
 import Modal from 'react-bootstrap/Modal';
 import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button';
 
 export default function UserSignUpIn(props) {
     const [showModal, setShow] = useState(false);
@@ -32,26 +30,22 @@ export default function UserSignUpIn(props) {
             <div onClick={handleYes} className="user-sign-div">
                 <span>{props.name}</span>
             </div>
-
             <Modal show={showModal} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <div>
-                        <button onClick={handkeSignIn}>Sign In</button>
-                        <button onClick={handkeSignUp}>Sign Up</button>
+                    <div className="header-btn-div">
+                        <button onClick={handkeSignIn} className={(showSignIn ? 'selected' : '')}>Sign In</button>
+                        <span>/</span>
+                        <button onClick={handkeSignUp} className={(!showSignIn ? 'selected' : '')}>Sign Up</button>
                     </div>
                 </Modal.Header>
                 <Modal.Body>
                     <InputGroup className="sign-in-body">
-                        <FormControl as="textarea" aria-label="With textarea"/>
-                        <div style={{ display: showSignIn ? 'block' : 'none' }}>
-                            <SignIn/>
+                        <div className="sign-comp-div" style={{ display: showSignIn ? 'flex' : 'none' }}>
+                            <SignIn />
                         </div>
-                        <div style={{ display: !showSignIn ? 'block' : 'none' }}>
-                            <SignUp/>
+                        <div className="sign-comp-div" style={{ display: !showSignIn ? 'flex' : 'none' }}>
+                            <SignUp />
                         </div>
-                        <Button variant="primary" type="submit" >
-                            Submit
-                        </Button>
                     </InputGroup>
                 </Modal.Body>
             </Modal>
