949a1656cab18ecd5f6295c6f9548911074682f4
Elnar  Sharifli <esharifli@homit.ca>
2020-05-02 16:19:48 -0600

    user input functionality added FE - profile info

--- src/Components/ProfileUserInfo.js
+++ src/Components/ProfileUserInfo.js
@@ -10,10 +10,10 @@ import Indsutries from './Lists/Industries'
 import UserStats from './UserStats'
 import Locations from './Lists/Locations'
 
-import UserProfPic from '../images/users/profile-pictures/elnarsharifli.jpg'
+// import UserProfPic from '../images/users/profile-pictures/elnarsharifli.jpg'
 
 export default function ProfileUserInfo() {
-    const [editInfo, setEditInfo] = useState(false)
+    let userInfoBE = {};
 
     let firstName = useRef(null);
     let lastName = useRef(null);
@@ -23,31 +23,16 @@ export default function ProfileUserInfo() {
     let industry = null;
     let location = null;
 
-    let userInfo = {};
-
-    // check session and retrieve user info
-    if (true) {
-        userInfo = {
-            firstName: "Elnar",
-            lastName: "Sharifli",
-            email: "elnarsharifli@gmail.com",
-            jobTitle: "Reporting Analyst",
-            location: {
-                "id": "1",
-                "value": "Calgary, Canada"
-            },
-            industry: {
-                "id": "2",
-                "value": "Investment Management",
-                "__typename": "Pair"
-            }
-        }
-
-    }
+    const [editInfo, setEditInfo] = useState(false);
 
     const [stateObj, setMessage] = useState({
         firstNameMessage: null,
-        lastNameMessage: null
+        lastNameMessage: null,
+        emailMessage: null,
+        jobTitleMessage: null,
+        industryMessage: null,
+        locationMessage: null,
+        passMessage: null
     });
 
     const constraints = {
@@ -71,9 +56,8 @@ export default function ProfileUserInfo() {
             presence: { allowEmpty: false }
         },
         email: {
-            email: {
-                message: "Please enter valid email"
-            }
+            email: true,
+            presence: { allowEmpty: false }
         },
         password: {
             length: {
@@ -81,6 +65,27 @@ export default function ProfileUserInfo() {
             }
         }
     };
+
+    // check session and retrieve user info
+    if (true) {
+        userInfoBE = {
+            firstName: "Elnar",
+            lastName: "Sharifli",
+            email: "elnarsharifli@gmail.com",
+            jobTitle: "Reporting Analyst",
+            profileImg: "https://www.telegraph.co.uk/content/dam/technology/2017/11/01/emoji_update_2017_1_trans_NvBQzQNjv4BqqVzuuqpFlyLIwiB6NTmJwfSVWeZ_vEN7c6bHu2jJnT8.png?imwidth=450",
+            location: {
+                "id": "1",
+                "value": "Calgary, Canada"
+            },
+            industry: {
+                "id": "2",
+                "value": "Investment Management",
+                "__typename": "Pair"
+            }
+        }
+
+    }
 
     function handleChangeJobTitle(newValue) {
         jobTitle = newValue;
@@ -95,24 +100,26 @@ export default function ProfileUserInfo() {
     }
 
     function handleHideEditInfo() {
-        setEditInfo(true);
+        if (editInfo) {
+            window.location.reload();
+        }
+        setEditInfo(!editInfo);
     }
 
     function handleShowEditInfo() {
         setEditInfo(false);
-        updateUserInfo();
     }
 
     // const [callUpdateUserInfo, { loading, error, data }] = useMutation(USER_UPDATE_INFO);
 
     const updateUserInfo = e => {
         let check = Validate({
-            firstName: firstName.current.value,
-            lastName: lastName.current.value,
-            jobTitle: jobTitle,
-            industry: industry,
-            location: location,
-            email: email.current.value,
+            firstName: (firstName.current.value || firstName.current.value === '') ? firstName.current.value : userInfoBE.firstName,
+            lastName: (lastName.current.value || lastName.current.value === '') ? lastName.current.value : userInfoBE.lastName,
+            jobTitle: jobTitle ? jobTitle : userInfoBE.jobTitle,
+            industry: industry ? industry : userInfoBE.industry,
+            location: location ? location : userInfoBE.location,
+            email: (email.current.value || email.current.value === '') ? email.current.value : userInfoBE.email,
             password: password.current.value
         }, constraints);
 
@@ -121,7 +128,7 @@ export default function ProfileUserInfo() {
                 ...prevState,
                 firstNameMessage: check && check.firstName ? "Can only contain letters" : null,
                 lastNameMessage: check && check.lastName ? "Can only contain letters" : null,
-                usernameMessage: check && check.email ? "Please enter valid email" : null,
+                usernameMessage: check && check.email[0] ? "Please enter valid email" : null,
                 jobTitleMessage: check && check.jobTitle ? "Can only contain letters" : null,
                 industryMessage: check && check.industry ? "Required" : null,
                 locationMessage: check && check.location ? "Required" : null,
@@ -136,6 +143,7 @@ export default function ProfileUserInfo() {
             //         pwd: password.current.value
             //     }
             // });
+            handleShowEditInfo();
         }
 
     };
@@ -144,30 +152,31 @@ export default function ProfileUserInfo() {
         <Container fluid className="user-info-container">
             <Row>
                 <Col sm={3} className="img-col">
-                    <img src={UserProfPic} className="user-prof-pic" alt="User Profile" />
+                    <img src={userInfoBE.profileImg} className="user-prof-pic" alt="User Profile" />
                     <UserStats />
                     <button className="user-prof-btn picture-btn">Edit</button>
                 </Col>
                 <Col sm={9} className="info-col">
                     <div className="input-btn-section">
-                        <button className={(!editInfo ? 'user-prof-btn info-edit-btn' : 'hide')} onClick={handleHideEditInfo}>Edit</button>
+                        <button className="user-prof-btn info-edit-btn"
+                            onClick={handleHideEditInfo}>{!editInfo ? 'Edit' : 'Cancel'}</button>
                         <div className="input-section">
                             <div className="user-names-div">
 
                                 <div className={(!stateObj.firstNameMessage ? 'user-input' : 'user-input error')}>
                                     <label>First Name</label>
-                                    <input name="first-name"
-                                        value={userInfo.firstName}
+                                    <input name="firstName"
+                                        defaultValue={userInfoBE.firstName}
                                         ref={firstName}
                                         disabled={!editInfo}
                                         type="text" />
                                     <span className="helper-txt">{stateObj.firstNameMessage}</span>
                                 </div>
 
-                                <div className={(!stateObj.firstNameMessage ? 'user-input lname' : 'user-input error lname')}>
+                                <div className={(!stateObj.lastNameMessage ? 'user-input lname' : 'user-input error lname')}>
                                     <label>Last Name</label>
-                                    <input name="first-name"
-                                        value={userInfo.lastName}
+                                    <input name="lastName"
+                                        defaultValue={userInfoBE.lastName}
                                         ref={lastName}
                                         disabled={!editInfo}
                                         type="text" />
@@ -176,19 +185,19 @@ export default function ProfileUserInfo() {
                             </div>
 
                             <Indsutries thisDisabled={!editInfo}
-                                thisValue={userInfo.industry}
+                                thisValue={userInfoBE.industry}
                                 helperText={stateObj.industryMessage}
                                 onChange={handleChangeIndustry}
                                 thisClassName="autocomplete" />
 
                             <Occupations thisDisabled={!editInfo}
-                                thisValue={userInfo.jobTitle}
+                                thisValue={userInfoBE.jobTitle}
                                 helperText={stateObj.jobTitleMessage}
                                 onChange={handleChangeJobTitle}
                                 thisClassName="autocomplete" />
 
                             <Locations thisDisabled={!editInfo}
-                                thisValue={userInfo.location}
+                                thisValue={userInfoBE.location}
                                 helperText={stateObj.locationMessage}
                                 onChange={handleChangeLocation}
                                 thisClassName="autocomplete" />
@@ -196,7 +205,7 @@ export default function ProfileUserInfo() {
                             <div className={(!stateObj.usernameMessage ? 'user-input email' : 'user-input error email')}>
                                 <label>Email</label>
                                 <input name="user-email"
-                                    value={userInfo.email}
+                                    defaultValue={userInfoBE.email}
                                     ref={email}
                                     disabled={!editInfo}
                                     type="email" />
@@ -212,7 +221,8 @@ export default function ProfileUserInfo() {
                                 <span className="helper-txt">{stateObj.passMessage}</span>
                             </div>
                         </div>
-                        <button className={(!editInfo ? 'hide' : 'user-prof-btn info-update-btn')} onClick={handleShowEditInfo}>Update</button>
+                        <button className={(!editInfo ? 'hide' : 'user-prof-btn info-update-btn')} 
+                            onClick={updateUserInfo}>Update</button>
                     </div>
                 </Col>
             </Row>
