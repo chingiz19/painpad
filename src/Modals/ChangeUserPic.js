50379324d88bae2d967659531f7d5ab221a272d6
Elnar  Sharifli <esharifli@homit.ca>
2020-05-05 16:53:03 -0600

    updated

--- src/Modals/ChangeUserPic.js
+++ src/Modals/ChangeUserPic.js
@@ -1,14 +1,14 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import './ChangeUserPic.css';
+import Loading from '../Components/Loading'
+import gql from 'graphql-tag';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { useMutation } from '@apollo/react-hooks';
 import Modal from 'react-bootstrap/Modal';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Cropper from 'react-easy-crop'
 import getCroppedImg from '../Components/Helpers/CropImage'
-import gql from 'graphql-tag';
-import { useLazyQuery } from '@apollo/react-hooks';
-import { useMutation } from '@apollo/react-hooks';
 import axios from 'axios';
-import Loading from '../Components/Loading'
 
 export default function ChangePassword(props) {
 
@@ -22,6 +22,8 @@ export default function ChangePassword(props) {
     const [imageType, setImageType] = useState('');
     const [imageName, setImageName] = useState('');
     const [imageLoading, setImageLoading] = useState(false);
+
+    const [imageUploadMsg, setImageUploadMsg] = useState('');
 
     const [cropObj, setCropObj] = useState({
         image: props.userPic,
@@ -55,7 +57,7 @@ export default function ChangePassword(props) {
         }
     });
 
-    let [callGetSignS3] = useLazyQuery(GET_SIGNS3, {
+    const [callGetSignS3] = useLazyQuery(GET_SIGNS3, {
         onCompleted: data => {
             var options = {
                 headers: {
@@ -96,11 +98,18 @@ export default function ChangePassword(props) {
         });
     }
 
-    const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
-        setCroppedAreaPixels(croppedAreaPixels)
-    }, [])
+    const onZoomChange = zoom => {
+        setCropObj({
+            ...cropObj,
+            zoom: zoom
+        });
+    }
 
-    const uploadPhoto = useCallback(async () => {
+    const onCropComplete = (croppedArea, croppedAreaPixels) => {
+        setCroppedAreaPixels(croppedAreaPixels)
+    };
+
+    const uploadPhoto = async () => {
         try {
             setImageLoading(true);
             const croppedImage = await getCroppedImg(
@@ -118,20 +127,20 @@ export default function ChangePassword(props) {
         } catch (e) {
             console.error(e)
         }
-    }, [croppedAreaPixels])
-
-    const onZoomChange = zoom => {
-        setCropObj({
-            ...cropObj,
-            zoom: zoom
-        });
-    }
+    };
 
     const handleImageUpload = e => {
         const [file] = e.target.files;
-
+        
         setImageType(file.name.split('.')[1]);
         setImageName((props.userId * 583).toString());
+
+        if (file.size > 2000000) {
+            setImageUploadMsg("Maxium file size 2 MB.");
+            return;
+        } else {
+            setImageUploadMsg("");
+        }
 
         if (file) {
             const reader = new FileReader();
@@ -156,7 +165,7 @@ export default function ChangePassword(props) {
                 </Modal.Header>
                 <Modal.Body>
                     <InputGroup className="CP-body">
-                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                        <div className="body-image-upload">
                             <input
                                 id="image-upload"
                                 type="file"
@@ -187,17 +196,19 @@ export default function ChangePassword(props) {
                             </div>
                         </div>
 
+                        <span className={imageUploadMsg ? 'error-msg' : 'none'}>{imageUploadMsg}</span>
+
                         {!showRemove
-                            ? <button className="btn-choose-pic" 
+                            ? <button className="btn-choose-pic"
                                 onClick={() => document.getElementById("image-upload").click()}>Choose photo</button>
                             : (
                                 (imageLoading || dataPostUserInfo)
                                     ? <Loading done={dataPostUserInfo} loading={imageLoading} />
-                                    : <button className="btn-upload-pic" onClick={uploadPhoto}>Upload photo</button>
+                                    : <button className="btn-upload-pic" onClick={uploadPhoto}>Update my photo</button>
                             )
                         }
 
-                        <button className={((showRemove && !imageLoading && !dataPostUserInfo) ? 'btn-remove' : 'none')} 
+                        <button className={((showRemove && !imageLoading && !dataPostUserInfo) ? 'btn-remove' : 'none')}
                             onClick={handleRemove}>X</button>
                     </InputGroup>
                 </Modal.Body>
