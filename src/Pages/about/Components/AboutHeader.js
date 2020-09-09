400c9e504a2a62050cdc5ea00e3e5d540e7a1787
Elnar  Sharifli <esharifli@homit.ca>
2020-09-09 00:09:14 -0600

    about page header updated

--- src/Pages/about/Components/AboutHeader.js
+++ src/Pages/about/Components/AboutHeader.js
@@ -6,7 +6,7 @@ import LogoTransperent from '../../../images/logos/logo_transparent.png';
 export default function AboutHeader() {
 
     return (
-        <div className="main-header">
+        <div className="main-header about">
             <img src={LogoTransperent} className="header-logo" alt="Transperent Logo" />
         </div>
     );
